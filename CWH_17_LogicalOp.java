3aeceb98587ca9d827e65e4b6f76610e2d82dde5
aayushhh12 <117062858+aayushhh12@users.noreply.github.com>
2024-01-20 23:30:53 +0530

    Add files via upload

---
+++ CWH_17_LogicalOp.java
@@ -0,0 +1,27 @@
+public class CWH_17_LogicalOp {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+
+        // for AND logical expression
+        /* if(a && b && c){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        } */
+
+        // for OR logical expression
+        /* if (a || b || c) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        } */
+
+        // for NOT logical expression
+        System.out.println(!a);
+        System.out.println(!b);
+        System.out.println(!c);
+    }
+}
