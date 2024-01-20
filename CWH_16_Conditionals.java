c9aac74ef6c79527500ad8997b943c3bb5e898da
aayushhh12 <117062858+aayushhh12@users.noreply.github.com>
2024-01-20 23:30:05 +0530

    Add files via upload

---
+++ CWH_16_Conditionals.java
@@ -0,0 +1,14 @@
+public class CWH_16_Conditionals {
+    public static void main(String[] args) {
+        int age = 18;
+        if(age>18){
+            System.out.println("Yes boi drive it!");
+        }
+        else{
+            System.out.println("No bro Sorry!");
+        }
+
+        // Here one point is that agar else na bhi dala to bhi koi dikkat nahi hogi bss false statement print nahi hogi.
+
+    }
+}
