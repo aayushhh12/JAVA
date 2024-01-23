dbf6bd1808754ed705ab0206b806d294e4053310
aayushhh12 <117062858+aayushhh12@users.noreply.github.com>
2024-01-23 12:24:28 +0530

    Add files via upload

---
+++ CWH_21_Loops.java
@@ -0,0 +1,11 @@
+public class CWH_21_Loops {
+    public static void main(String[] args) {
+        System.out.println("Using Loops: ");
+        int i = 100;
+        while (i<=200){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Finish Running while loop!");
+    }
+}
